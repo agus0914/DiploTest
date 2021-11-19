d42c8ab592ca0b8b2805547268d0c5ac057d6f3e
agustin <agustin0914@gmail.com>
2021-11-19 18:59:02 -0300

    agrego class Led con metodos begin(), on(), off()

---
+++ led.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Arduino.h>
+#include <stdint.h>
+
+
+class Led
+{
+private:
+    uint16_t pin;
+public:
+    void begin(uint16_t p)
+    {
+        pin = p;
+        pinMode(pin,OUTPUT);
+    }
+    void on()
+    {
+        digitalWrite(pin,HIGH);
+    }
+    void of()
+    {
+        digitalWrite(pin,LOW);
+    }
+};
